b7d9b82abfe5974c88b3497436ebc05a713b8e72
yurikong <jackdu2013@163.com>
2022-07-05 14:32:08 +0800

    test ant-design-vue import on demand

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Button type="primary">Ant Design BTN</Button>
   </div>
 </template>
+
+<script setup lang="ts">
+import { Button } from "ant-design-vue"
+</script>
